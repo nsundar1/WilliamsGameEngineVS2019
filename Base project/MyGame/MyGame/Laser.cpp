6beb17355b2a96256bd33df545aed250031e34b6
Neel Sundar <sundarn@rsd407.org>
2021-04-26 12:47:43 -0700

    Doing laser

---
+++ Base project/MyGame/MyGame/Laser.cpp
@@ -0,0 +1,40 @@
+#include "Laser.h"
+
+
+
+
+
+const float SPEED = 1.1f;
+
+Laser::Laser(sf::Vector2f pos) {
+	sprite_.setTexture(GAME.getTexture("Resources/laser.png"));
+	sprite_.setPosition(pos);
+	assignTag("laser");
+
+
+
+
+	
+}
+void Laser::draw() {
+	GAME.getRenderWindow().draw(sprite_);
+
+}
+
+void Laser::update(sf::Time& elapsed) {
+	int msElapsed = elapsed.asMilliseconds();
+	sf::Vector2f pos = sprite_.getPosition();
+
+	if (pos.x > GAME.getRenderWindow().getSize().x) {
+
+		makeDead();
+
+	}
+	else {
+		sprite_.setPosition(sf::Vector2f(pos.x + SPEED * msElapsed, pos.y));
+	}
+}
+
+
+//SHIP.h page 25
+
